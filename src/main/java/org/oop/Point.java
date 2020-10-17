c858b31a31edadd9451bb70205bd2a081e5acf89
krishna sindhur maruvala <krishna.sindhur@thoughtworks.com>
2020-10-18 00:05:17 +0530

    refactored the Point and DistanceAndDirectionCalculator class

--- src/main/java/org/oop/Point.java
+++ src/main/java/org/oop/Point.java
@@ -4,24 +4,17 @@ public class Point {
     private double x;
     private double y;
 
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
+    }
+
+    public double calculateDistance(Point point) {
+        return Math.sqrt(Math.pow(point.x - this.x, 2) + Math.pow(point.y - this.y, 2));
+    }
+
+    public  double calculateDirection(Point point) {
+        return Math.atan2(point.y - this.y, point.x - this.x);
+
     }
 }
